b11fce7ec9c8c6cddda79aa672ba59954541682a
komir-jpg <mirkoe152@gmail.com>
2023-12-25 16:08:18 +0100

    gestione login

--- OO-Project/src/main/java/DemoUninaSN/OO_Project/Controller.java
+++ OO-Project/src/main/java/DemoUninaSN/OO_Project/Controller.java
@@ -1,22 +1,28 @@
 package DemoUninaSN.OO_Project;
 
 public class Controller {
-	Demo LoginFrame;
-	Frame2 LoginSuccessoframe;
+	Login LoginFrame;
+	HomePage LoginSuccessoframe;
+	RegisterUserFrame RegisterFrame;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Controller();
 	}
 	public Controller() {
-		LoginFrame = new Demo(this);
+		LoginFrame = new Login(this);
 		LoginFrame.setVisible(true);
 	}
 	
-	public void setFrame2() {
-		LoginSuccessoframe = new Frame2(this);
+	public void setHomePageFrame() {
+		LoginSuccessoframe = new HomePage(this);
 		LoginFrame.setVisible(false);
 		LoginSuccessoframe.setVisible(true);
+	}
+	public void setRegisterFrame() {
+		RegisterFrame = new RegisterUserFrame();
+		RegisterFrame.setVisible(true);
+		LoginFrame.setVisible(false);
 	}
 
 }
