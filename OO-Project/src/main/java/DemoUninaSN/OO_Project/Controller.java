585eac77ad4b02bdb345fe592ee03c25089b241b
komir-jpg <mirkoe152@gmail.com>
2023-12-26 14:09:59 +0100

    Gui login

--- OO-Project/src/main/java/DemoUninaSN/OO_Project/Controller.java
+++ OO-Project/src/main/java/DemoUninaSN/OO_Project/Controller.java
@@ -1,12 +1,15 @@
 package DemoUninaSN.OO_Project;
 
+import java.awt.Point;
+
+import javax.swing.JFrame;
+
 public class Controller {
 	Login LoginFrame;
 	HomePage LoginSuccessoframe;
 	RegisterUserFrame RegisterFrame;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Controller();
 	}
 	public Controller() {
@@ -15,14 +18,27 @@ public class Controller {
 	}
 	
 	public void setHomePageFrame() {
+		Point point;
 		LoginSuccessoframe = new HomePage(this);
+		point = GetFramePosition(LoginFrame);
 		LoginFrame.setVisible(false);
 		LoginSuccessoframe.setVisible(true);
+		SetFramePosition(LoginSuccessoframe, point);
+		
 	}
 	public void setRegisterFrame() {
 		RegisterFrame = new RegisterUserFrame();
 		RegisterFrame.setVisible(true);
 		LoginFrame.setVisible(false);
 	}
+	private Point GetFramePosition(JFrame frame) {
+		Point point;
+		point = frame.getLocationOnScreen();
+		return point;
+	}
+	private void SetFramePosition(JFrame frame,Point point) {
+		frame.setLocation(point);
+	}
+	
 
 }
--- OO-Project/src/main/java/DemoUninaSN/OO_Project/HomePage.java
+++ OO-Project/src/main/java/DemoUninaSN/OO_Project/HomePage.java
@@ -16,18 +16,6 @@ public class HomePage extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					Frame2 frame = new Frame2();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
 
 	/**
 	 * Create the frame.
