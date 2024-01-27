7d1172173ec352fe4f45780f0be3a9cf9119192a
komir-jpg <mirkoe152@gmail.com>
2024-01-27 12:25:40 +0100

    tag post

---
+++ OO-Project/src/main/java/DemoUninaSN/OO_Project/Tag.java
@@ -0,0 +1,16 @@
+package DemoUninaSN.OO_Project;
+
+public class Tag {
+	private String category;
+	public Tag() {
+		
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+
+}
---
+++ OO-Project/src/main/java/DemoUninaSN/OO_Project/TagDAO.java
@@ -0,0 +1,55 @@
+package DemoUninaSN.OO_Project;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class TagDAO extends getIdDAO{
+	Connection connection;
+	Statement statement;
+	PreparedStatement preparedStatement;
+	
+	
+	public TagDAO(Connection myConnection) throws ClassNotFoundException, SQLException, IOException, RuntimeException {
+		super(myConnection);
+	}
+	
+	public void insertNewTag(Tag tag) {
+		String insertNewTag = "INSERT INTO progettobd_unina_social_network.TAG VALUES ("
+							  +"\'"+tag.getCategory()+"\'"+")";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(insertNewTag);
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public ArrayList<Tag> getTag(){
+		String getTag = "SELECT * FROM progettobd_unina_social_network.TAG";
+		try {
+			preparedStatement = connection.prepareStatement(getTag);
+			ResultSet queryRS = preparedStatement.executeQuery();
+			ArrayList<Tag> queryResultTag = new ArrayList<Tag>();
+			while(queryRS.next()) {
+				Tag tag = new Tag();
+				tag.setCategory(queryRS.getString("categoria"));
+				queryResultTag.add(tag);
+				queryRS.close();
+				preparedStatement.close();
+				return queryResultTag;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	
+}
